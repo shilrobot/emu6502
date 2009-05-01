e8fd40887cf1e7714c18aac259b25cbf3ab43725
shilbert <shilbert@6dcb506c-49f4-8c44-b148-53dce8eab73e>
2009-05-01 05:26:40 +0000

    * Added missing files.
    git-svn-id: http://shilbert.cjb.net/repos/private/trunk/csharp/Emu6502@2208 6dcb506c-49f4-8c44-b148-53dce8eab73e

---
+++ BreakpointManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Emu6502
+{
+    public class Breakpoint
+    {
+        public ushort Address { get; set; }
+        public bool Enabled;
+
+        public Breakpoint(ushort addr) 
+        {
+            Address = addr;
+            Enabled = true;
+        }
+    }
+
+    public class BreakpointManager
+    {
+        private Dictionary<ushort, Breakpoint> breakpoints = new Dictionary<ushort, Breakpoint>();
+
+        public Breakpoint SetBreakpoint(ushort addr)
+        {
+            if (breakpoints.ContainsKey(addr))
+                return breakpoints[addr];
+            else
+            {
+                Breakpoint bp = new Breakpoint(addr);
+                breakpoints[addr] = bp;
+                return bp;
+            }
+        }
+
+        public void ClearBreakpoint(ushort addr)
+        {
+            breakpoints.Remove(addr);
+        }
+
+        public Breakpoint GetBreakpoint(ushort addr)
+        {
+            Breakpoint bp = null;
+            if(breakpoints.TryGetValue(addr, out bp))
+                return bp;
+            else
+                return null;
+        }
+
+        public List<ushort> GetBreakpointAddresses()
+        {
+            return new List<ushort>(breakpoints.Keys);
+        }
+    }
+}
